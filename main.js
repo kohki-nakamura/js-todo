621d28e578caa1cdca21c394640e6a3593f838d9
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-09-20 00:42:32 +0900

    rewrite: rxjs

--- main.js
+++ main.js
@@ -1,46 +1,9 @@
-const addButton = document.querySelector('#addButton');
-addButton.addEventListener('click', () => {
-  showAlert('追加ボタンクリック', 'これで確認できました', 'もう閉じて良いですよ');
-});
+const subject = new rxjs.Subject();
+const { filter, map, scan, tap, mergeMap } = rxjs.operators;
 
-function showAlert(header, subheader, message) {
-  const alert = document.createElement('ion-alert');
-  alert.header = header;
-  alert.subHeader = subheader;
-  alert.message = message;
-  alert.buttons = [
-    {
-      text: 'Cancel',
-      role: 'cancel'
-    },
-    {
-      text: 'OK',
-      handler: () => {
-        const todo = {};
-        todo.title = document.querySelector('#new-todo').value;
-        todo.due = new Date(document.querySelector('#new-due').value);
-        todo.done = false;
-        todo.id = 'todo' + (new Date).getTime().toString();
-        todos.push(todo);
-        addToDo(todo);
-      }
-    }
-  ];
-  alert.inputs = [{
-    name: 'todo',
-    id: 'new-todo',
-    placeholder: 'やらなきゃいけないことは何？'
-  },
-  {
-    name: 'due',
-    id: 'new-due',
-    type: 'date',
-  }
-];
-
-  document.body.appendChild(alert);
-  return alert.present();
-}
+const WRITE_TODOS = '[ToDo Page] Load ToDos';
+const DELETE_TODOS = '[ToDo Page] Delete ToDo';
+const TOGGLE_STATUS = '[ToDo Page] Toggle ToDo Status';
 
 const todos = [
   {
@@ -99,35 +62,69 @@ const todos = [
   },
 ];
 
-writeAllToDos(todos);
+// 追加用のイベント処理
+subject.pipe(
+  filter(action => action.type === WRITE_TODOS),
+  mergeMap(action => rxjs.from(action.payload)),
+  tap(todo => addToDo(todo))
+).subscribe()
 
-function writeToDos(todos) {
-  const todoList = document.querySelector('#todos');
-  const doneList = document.querySelector('#done');
-  for (let todo of todos) {
-    const todoItem = document.createElement('ion-item')
-    todoItem.innerHTML = `
-      <ion-label>
-        <h2>${todo.title}</h2>
-        <h3>${todo.due.toDateString()}</h3>
-      </ion-label>
-      <ion-checkbox ${( todo.done ? 'checked' : '' )} color="primary" slot="start"></ion-checkbox>
-    `
-    if (todo.done) {
-      // 完了している時は、doneListへ
-      doneList.appendChild(todoItem);
-    } else {
-      // 完了していないものは、todoListへ
-      todoList.appendChild(todoItem);
-    }
-  }
+// 削除用のイベント処理
+subject.pipe(
+  filter(action => action.type === DELETE_TODOS),
+  map(action => action.payload),
+  tap(todo => document.querySelector(`#${todo.id}`).remove()),
+).subscribe()
+
+// トグル用のイベント処理
+subject.pipe(
+  filter(action => action.type === TOGGLE_STATUS),
+  map(action => action.payload),
+  tap(todo => subject.next({
+    type: DELETE_TODOS,
+    payload: todo,
+  })),
+  tap(todo => subject.next({
+    type: WRITE_TODOS,
+    payload: [todo]
+  }))
+).subscribe()
+
+// ToDo 追加用のアラート表示
+const addButton = document.querySelector('#addButton');
+rxjs.fromEvent(addButton, 'click')
+  .pipe(
+    tap(e => showAlert('タスク追加', '', 'やらなきゃいけないことは何？'))
+  ).subscribe()
+
+// todo 削除
+function onDeleteClicked(todoId) {
+  const todo = todos.find(el => el.id === todoId);
+  todos.splice(todos.findIndex(el => el.id === todoId), 1);
+  const action = {
+    type: DELETE_TODOS,
+    payload: todo
+  };
+  subject.next(action);
 }
 
-function writeAllToDos(todos) {
-  for (let todo of todos) {
-    addToDo(todo);
-  }
+// ToDo の Done 状態をトグルするイベント、リスナは onClick 属性で登録
+function onItemClicked(todoId) {
+  const todo = todos.find(el => el.id === todoId);
+  todo.done = !todo.done;
+  const action = {
+    type: TOGGLE_STATUS,
+    payload: todo
+  };
+  subject.next(action)
 }
+
+const action = {
+  type: WRITE_TODOS,
+  payload: todos
+};
+
+subject.next(action);
 
 function addToDo(todo) {
   const todoList = document.querySelector('#todos');
@@ -159,16 +156,4 @@ function addToDo(todo) {
     // 完了していないものは、todoListへ
     todoList.appendChild(todoItemWithSliding);
   }
-}
-
-function deleteToDo(todoId) {
-  todos.splice(todos.findIndex(el => el.id === todoId), 1);
-  document.querySelector(`#${todoId}`).remove();
-}
-
-function onItemClicked(todoId) {
-  const todo = todos.find(el => el.id === todoId); // 元のtodoデータを取得
-  todo.done = !todo.done; // todo の状態を変更
-  document.querySelector(`#${todoId}`).remove(); // 今表示されているところを削除
-  addToDo(todo); // addToDoを呼び出し
 }
